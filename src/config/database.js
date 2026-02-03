ce30b0b1f31f1224054f10ee0ddcb5d8c46ee336
Gowri239 <gowrimopuru14@gmail.com>
2026-02-03 21:14:24 +0530

    refactor: move credentials to env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://Gowri:KKkg%402329@namastenode.125zreo.mongodb.net/%20now%20,%20where%20can%20I%20find%20my%20dbusername%20and%20password/devTinder"
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
