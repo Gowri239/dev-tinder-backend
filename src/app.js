31998796073004bfa170323f4c2e31f5ceb5fcdd
Gowri239 <gowrimopuru14@gmail.com>
2026-02-03 21:22:02 +0530

    add: signup api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const connectDB = require("./Config/database");
+const User = require("./models/user");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.end("Server successfully running on port 3000");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Krishna",
+    lastName: "Koneti",
+    gender: "male",
+    age: 30,
+    emailId: "kk@gmail.com",
+    password: "abcde",
+  });
+
+  await user.save();
+  res.send("User Added Successfully");
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     type: String,
   },
   age: {
-    type: String,
+    type: Number,
   },
   emailId: {
     type: String,
