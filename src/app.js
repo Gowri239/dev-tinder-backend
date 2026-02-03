91665f1ed4485764a467df0bc60d10ee1941bb96
Gowri239 <gowrimopuru14@gmail.com>
2026-02-03 20:56:04 +0530

    add: database connection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./Config/database");
 
 const app = express();
 
@@ -6,4 +7,13 @@ app.use("/test", (req, res) => {
   res.end("Server successfully running on port 3000");
 });
 
-app.listen(3000);
+connectDB()
+  .then(() => {
+    console.log("Database connection established");
+    app.listen(3000, () => {
+      console.log("Server successfully running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection error", err);
+  });
